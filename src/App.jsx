3d5738ffc44c1a2522e25c83937b12da0deca404
김문종 <hushkmj@naver.com>
2025-05-01 11:40:20 +0900

    implementing Home page

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,21 @@ import Notfound from "./pages/Notfound";
 const mockData = [
   {
     id: 1,
-    createdData: new Date().getTime(),
+    createdDate: new Date("2025-05-06"),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdData: new Date().getTime(),
+    createdDate: new Date("2025-04-30"),
     emotionId: 2,
     content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2025-04-10"),
+    emotionId: 3,
+    content: "3번 일기 내용",
   },
 ];
 
@@ -37,8 +43,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,20 +1,25 @@
 import "./DiaryItem.css";
 import { getEmotionImage } from "../util/get-emotion-image";
 import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
-const DiaryItem = () => {
-  const emotionId = 1;
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const nav = useNavigate();
+
   return (
     <div className="DiaryItem">
-      <div className={`img_section img_section_${emotionId}`}>
+      <div
+        className={`img_section img_section_${emotionId}`}
+        onClick={() => nav(`/diary/${id}`)}
+      >
         <img src={getEmotionImage(emotionId)} />
       </div>
-      <div className="info_section">
-        <div className="created_date">{new Date().toLocaleDateString()}</div>
-        <div className="content">Diary Content</div>
+      <div className="info_section" onClick={() => nav(`/diary/${id}`)}>
+        <div className="created_date">{createdDate.toLocaleDateString()}</div>
+        <div className="content">{content}</div>
       </div>
       <div className="button_section">
-        <Button text={"수정하기"} />
+        <Button text={"수정하기"} onClick={() => nav(`/edit/${id}`)} />
       </div>
     </div>
   );
