3ecda6b6643f0f2f407493637528f9a5f8125da2
김문종 <hushkmj@naver.com>
2025-05-04 08:15:19 +0900

    implementing New page UI

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,17 @@
+import Button from "../components/Button";
+import Header from "../components/Header";
+import Editor from "./Editor";
+
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default New;
